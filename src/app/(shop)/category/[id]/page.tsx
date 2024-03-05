92a428aa95d72636446c115888362c8749cc746b
Lu1sEnriquez <luis.lavz.2300@gmail.com>
2024-03-04 18:14:41 -0700

    add page/category with filters [men, women, kid]

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,22 +1,41 @@
+import { ProductGrid, Title } from "@/components";
+import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
+import { Category } from "@/interfaces/product.interface";
 
-interface Props{
-  params : {
-    id: string;
-  }
+interface Props {
+  params: {
+    id: Category;
+  };
 }
 
-export default function CategoryPage({params}: Props) {
+/** FORMAS DE TIPAR
+ * {[key: ValidCategories] or Record<ValidCategories, string>}: indica que el parámetro debe ser de tipo Valid Categories
+ */
+const labels: Record<Category,string> = {
+  'men': 'para Hombres',
+  kid: 'para Mujeres',
+  women: 'para Niños',
+  unisex: 'para unisex'
 
-  const {id} = params
+}
 
-  if(id === 'kids'){
-    notFound();
-  }
+
+export default function CategoryPage({ params }: Props) {
+  const { id } = params;
+
+  // if(id === 'kids'){
+  //   notFound();
+  // }
+  const products = initialData.products.filter(
+    (product) => product.gender === id
+  );
 
   return (
-    <div>
-      <h1>Category Page {id}</h1>
-    </div>
+    <> 
+      <Title title={`Artículos de ${labels[id]}`} subtitle="Todos Los Productos" className="mb-2" />
+
+      <ProductGrid product={products} />
+    </>
   );
 }
